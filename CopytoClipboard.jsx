6dbdcd31af9e9fec1c12d533a5956f09b735738e
Dilpreet <92905896+dfordp@users.noreply.github.com>
2023-02-24 12:37:57 +0530

    Create CopytoClipboard.jsx

---
+++ CopytoClipboard.jsx
@@ -0,0 +1,6 @@
+ const onPressed = () => {
+        if(window !== undefine && colorPalette.length === 5) {
+            window.navigator.clipboard.writeText(`${Element(s) to be copied});
+            toast.success("Copied to clipboard");
+        }
+    };
